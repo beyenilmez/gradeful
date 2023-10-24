86faf1f24f8330682781e9f6d27f6f575fe62cdc
Bedirhan Yenilmez <76536654+beyenilmez@users.noreply.github.com>
2023-10-24 14:05:17 +0300

    Added drag support

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import SettingsPopup from "./components/SettingsPopup";
 import Term from "./components/Term";
+import "./utils/drag.js";
 
 document.body.className = 'bg-slate-250 dark:bg-slate-750 dark:text-slate-200 text-slate-800';
 
@@ -13,9 +14,9 @@ function App() {
       <NavBar setShowSettings={setShowSettings} />
       <SettingsPopup showSettings={showSettings} setShowSettings={setShowSettings} />
       <div id="terms" class="drop_container grid gap-7 grid-cols-1 md:grid-cols-2 m-8 dark:text-slate-200 text-slate-800">
-        <Term dataid={"1"} name={"Term 1"} />
-        <Term dataid={"2"} name={"Term 2"} />
-        <Term dataid={"3"} name={"Term 3"} />
+        <Term dataid={"1"} name={"Term 1"} ></Term>
+        <Term dataid={"2"} name={"Term 2"} ></Term>
+        <Term dataid={"3"} name={"Term 3"} ></Term>
       </div>
     </div>
   );
