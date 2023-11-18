a458b994080e901743f79a585eeb7ea90b03e8d5
Bedirhan Yenilmez <76536654+beyenilmez@users.noreply.github.com>
2023-11-18 16:55:53 +0300

    Replaced drag.js with react-sortablejs
    -Mobile dragging not working as intended

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import SettingsPopup from "./components/SettingsPopup";
 import Term from "./components/Term";
-import "./utils/drag.js";
+import { ReactSortable } from "react-sortablejs";
+import './css/drag.css';
+
+const termList = [
+  {
+    termName: "Term 1",
+    termId: "1"
+  },
+  {
+    termName: "Term 2",
+    termId: "2"
+  },
+  {
+    termName: "Term 3",
+    termId: "3"
+  },
+];
 
 document.body.className = 'bg-slate-250 dark:bg-slate-750 dark:text-slate-200 text-slate-800';
 
 function App() {
   let [showSettings, setShowSettings] = useState(false);
+  const [draggableTermList, setDraggableTermList] = useState(termList);
+
 
   return (
     <div>
       <NavBar setShowSettings={setShowSettings} />
       <SettingsPopup showSettings={showSettings} setShowSettings={setShowSettings} />
-      <div id="terms" class="drop_container grid gap-7 grid-cols-1 md:grid-cols-2 m-8 dark:text-slate-200 text-slate-800">
-        <Term dataid={"1"} name={"Term 1"} ></Term>
-        <Term dataid={"2"} name={"Term 2"} ></Term>
-        <Term dataid={"3"} name={"Term 3"} ></Term>
-      </div>
+      <ReactSortable
+      //Dragging mobile problem
+        chosenClass="sortable-chosen"
+        className="drop_container grid gap-7 grid-cols-1 md:grid-cols-2 m-8 dark:text-slate-200 text-slate-800"
+        dragClass="sortable-drag"
+        ghostClass="sortable-ghost"
+        list={draggableTermList}
+        setList={setDraggableTermList}
+        animation="300"
+        delay="115"
+        easing="ease-out"
+      >
+        {draggableTermList.map(item => (
+          <Term dataid={item.termId} name={item.termName} ></Term>
+        ))}
+      </ReactSortable>
     </div>
   );
 }
