1459a9ff12d62ded80be721f2c5a48d2da3b7c66
Bedirhan Yenilmez <76536654+beyenilmez@users.noreply.github.com>
2023-12-26 00:25:13 +0300

    bugfix

--- src/components/Class.jsx
+++ src/components/Class.jsx
@@ -224,7 +224,11 @@ function Class({ id, termId, name, children, isActive, setActive }) {
                             uni.load(universityData);
 
                             setEditJSON({ ...editJSON, [id]: uni.getSemesterById(termId).getClassById(id) });
+
                             setActive(true);
+                            setTimeout(() => {
+                                setContentHeight(`100%`);
+                            }, 50);
                         }}
                         onClick={(event) => event.stopPropagation()}
                         className={`h-7 mr-0.5 flex items-center justify-center dark:hover:bg-slate-650 hover:bg-slate-350 dark:active:bg-slate-600 active:bg-slate-400 ${editJSON[id] === undefined ? 'w-7' : 'w-0'}`}
