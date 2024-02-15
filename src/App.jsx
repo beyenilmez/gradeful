62e2c5cc3068fd0c8e382abbc1bbac8423088497
Bedirhan Yenilmez <76536654+beyenilmez@users.noreply.github.com>
2024-02-15 15:07:04 +0300

    #22 - fix scores not updating on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import LoadURLPopup from "./components/LoadURLPopup";
 import Grid from "./components/Grid";
 import { InactiveProvider } from "./components/InactiveContext";
 import QucikStartWindow from "./components/QuickStartWindow";
+import { useUniData } from "./components/UniContext";
 
 document.body.className = 'bg-slate-250 dark:bg-slate-750 dark:text-slate-200 text-slate-800 slate';
 
@@ -13,11 +14,13 @@ function App() {
   const [showSettings, setShowSettings] = useState(false);
   const [showExportURLPopup, setShowExportURLPopup] = useState(false);
 
+  const {refresh} = useUniData();
+
   return (
     <React.Fragment>
       <NavBar setShowSettings={setShowSettings} setShowExportURLPopup={setShowExportURLPopup} />
       <InactiveProvider>
-        <Grid />
+        <Grid key={refresh} />
       </InactiveProvider>
       <SettingsWindow showSettingsPopup={showSettings} setShowSettingsPopup={setShowSettings} />
       <ExportURLPopup showExportURLPopup={showExportURLPopup} setShowExportURLPopup={setShowExportURLPopup} />
